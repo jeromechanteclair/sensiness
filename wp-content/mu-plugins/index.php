c02a622ebf40aad4d6b4501de90cad9dde305d47
jerome <jerome.chanteclair@gmail.com>
2023-04-20 15:35:18 +0200

    --

--- wp-content/mu-plugins/index.php
+++ wp-content/mu-plugins/index.php
@@ -6,7 +6,7 @@
  * Description:      Affiche les FAQ au format opengraph optimisé
  * Version:           1.0.1
  * Author:            Jérôme Chanteclair
- * Author URI:        
+ * Author URI:        https://jerome-chanteclair.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
