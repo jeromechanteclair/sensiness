a9cc784efe2610b589ccc9a4680a30f1e7221662
jerome <jerome.chanteclair@gmail.com>
2023-04-20 15:34:42 +0200

    plugin name

--- wp-content/mu-plugins/index.php
+++ wp-content/mu-plugins/index.php
@@ -2,21 +2,14 @@
 
 /**
  * @wordpress-plugin
- * Plugin Name:       150 bar locator
- * Plugin URI:        https://bitbucket.org/pernod-ricard/wordpress-pr-common-libraries
- * Description:       Display a map of bars
+ * Plugin Name:       FAQ
+ * Description:      Affiche les FAQ au format opengraph optimisé
  * Version:           1.0.1
- * Author:            Pernod Ricard
- * Author URI:        https://developer.p-r.io
+ * Author:            Jérôme Chanteclair
+ * Author URI:        
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       150-bar-locator
- * Domain Path:       /languages
  */
 
 require(WPMU_PLUGIN_DIR . '/CPT/index.php');
-// require(WPMU_PLUGIN_DIR . '/blocks/ateliercrepus/plugin.php');
-// require(WPMU_PLUGIN_DIR . '/Sendinblue/index.php');
-// require(WPMU_PLUGIN_DIR . '/WooNotice/index.php');
-// require(WPMU_PLUGIN_DIR . '/CustomModal/index.php');
-// require(WPMU_PLUGIN_DIR . '/Maintenance/index.php');
+
