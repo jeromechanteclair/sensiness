e198aa3993d48460769463006bbd53c036420890
jerome <jerome.chanteclair@gmail.com>
2023-06-12 14:46:40 +0200

    build

--- wp-content/themes/hello-elementor-child/assets/js/scroll.js
+++ wp-content/themes/hello-elementor-child/assets/js/scroll.js
@@ -1,14 +1,15 @@
 function scroll() {
-    if($(document).find('.single-product .cart').length>0){
+    if ($(document).find('.single-product .cart').length > 0) {
 
-    
-    var formtop = $('.single-product .cart').offset().top;
-    var formtrigger = formtop + $('.single-product .cart').outerHeight();
-    var formtrigger =  $('.summary').outerHeight();
-            let summaryheigth = $('.summary').outerHeight(true);
 
-               $('.summary').css('min-height',summaryheigth+'px')
-}
+        var formtop = $('.single-product .cart').offset().top;
+        var formtrigger = formtop + $('.single-product .cart').outerHeight();
+        var formtrigger = $('.summary').outerHeight();
+        let summaryheigth = $('.summary').outerHeight(true);
+
+        $('.summary').css('min-height', summaryheigth + 'px')
+    }
+
     function isInViewport($element) {
         let elementTop = $($element).offset().top;
         let elementBottom = elementTop + $($element).outerHeight();
@@ -21,7 +22,7 @@ function scroll() {
 
     $(window).scroll(function () {
         // console.log($(document).find('.video-container '));
-        if ($(document).find('.video-container ').length>0) {
+        if ($(document).find('.video-container ').length > 0) {
 
 
             if (isInViewport($('.video-container '))) {
@@ -35,14 +36,14 @@ function scroll() {
             }
         }
         if (formtrigger < $(window).scrollTop()) {
-                
+
 
 
             let reworked = $('.single-product .cart').outerHeight();
             if (!$('.single-product .cart').hasClass('sticky')) {
                 // setTimeout(() => {
 
-                    $('.single-product .cart').addClass('sticky')
+                $('.single-product .cart').addClass('sticky')
                 // }, 500);
 
             }
@@ -54,7 +55,7 @@ function scroll() {
                 // $('.single-product .cart').removeClass('sticky')
                 // setTimeout(() => {
 
-                    $('.single-product .cart').removeClass('sticky')
+                $('.single-product .cart').removeClass('sticky')
                 // }, 500);
             }
 
